084059ce7869ac713c23541c7503bddadd70aca2
satoriomeditation <guidedmeditationnet@gmail.com>
2025-11-19 07:39:17 +0100

    Fix: remove BuyMeACoffeeWidget import

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { supabase } from '../supabaseClient'
-import BuyMeACoffeeWidget from '../components/BuyMeACoffeeWidget'
+
 import BuyMeACoffeeButton from '../components/BuyMeACoffeeButton'
 
 
